b21129b579c9295552a41950395031a6310c85de
Nohkwanglae <ilpus@naver.com>
2025-05-13 18:30:27 +0900

    modified:   "SECTOR_ETF/sectorETF_\353\263\200\353\217\231\354\204\261\353\217\214\355\214\214_Backtest_Fin.py" 	modified:   "SECTOR_ETF/\353\263\200\353\217\231\354\204\261\353\217\214\355\214\214Result.xlsx"

--- SECTOR_ETF/sectorETF_변동성돌파_Backtest_Fin.py
+++ SECTOR_ETF/sectorETF_변동성돌파_Backtest_Fin.py
@@ -245,9 +245,9 @@ class run_back_test:
         return result
 
 # 변수설정 #
-file_name = 'KOSDAQ 100.xlsx' # 직전 1개월간 거래대급 상위 국내섹터별 ETF 4개 #1,2위 2000, 3,4위 1000
+file_name = 'TIGER 화장품.xlsx' # 직전 1개월간 거래대급 상위 국내섹터별 ETF 4개 #1,2위 2000, 3,4위 1000
 save_dir = 'C:/Users/GSR/Desktop/Python_project/git_folder/SECTOR_ETF'
-# KODEX 200.xlsx #4000 # KODEX 코스닥150.xlsx # KOSDAQ 100.xlsx #2000
+# KODEX 200.xlsx #4000 # KOSDAQ 100.xlsx #2000 # KODEX 코스닥150.xlsx
 # KODEX 2차전지산업.xlsx # KODEX 반도체.xlsx # KODEX 은행.xlsx# KODEX 자동차.xlsx 
 # PLUS K방산.xlsx# SOL 조선TOP3플러스.xlsx
 # TIGER 200 IT.xlsx # TIGER 200 중공업.xlsx # TIGER 리츠부동산인프라.xlsx # TIGER 헬스케어.xlsx # TIGER 화장품.xlsx
