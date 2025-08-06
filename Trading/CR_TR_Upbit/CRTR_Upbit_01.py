f68b125acf43699ec1c8845414925b21aef45156
zalankr <ilpus@naver.com>
2025-08-07 00:19:40 +0900

    modified:   Trading/CR_TR_Upbit/CRTR_Upbit_01.py 	new file:   Trading/CR_TR_Upbit/__pycache__/myUpbit.cpython-313.pyc 	new file:   Trading/CR_TR_Upbit/myUpbit.py 	modified:   Trading/CR_TR_Upbit/upnkr.txt 	new file:   Trading/CR_TR_Upbit/upnkr2.txt

--- Trading/CR_TR_Upbit/CRTR_Upbit_01.py
+++ Trading/CR_TR_Upbit/CRTR_Upbit_01.py
@@ -1,5 +1,16 @@
+import pyupbit
+import myUpbit
+
+# Upbit 토큰 불러오기
 with open("C:/Users/ilpus/Desktop/git_folder/Trading/CR_TR_Upbit/upnkr.txt") as f:
     access_key, secret_key = [line.strip() for line in f.readlines()]
 
-print(access_key)
-print(secret_key)
+# 업비트 접속
+upbit = pyupbit.Upbit(access_key, secret_key)
+balances = upbit.get_balances()
+
+# 테스트
+TotalMoney = myUpbit.GetTotalMoney(balances)
+print(TotalMoney)
+Money = myUpbit.GetTotalRealMoney(balances)
+print(Money)
