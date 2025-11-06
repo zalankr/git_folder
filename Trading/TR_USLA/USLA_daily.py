8ee0620f5df877450b24cd7a221149c4df87a327
zalankr <ilpus@naver.com>
2025-11-06 16:23:14 +0900

    modified:   Trading/TR_USLA/USLA_daily.py

--- Trading/TR_USLA/USLA_daily.py
+++ Trading/TR_USLA/USLA_daily.py
@@ -1,4 +1,5 @@
 import USLA_model
+import kakao_alert as KA
 from datetime import datetime
 
 # USLA모델 instance 생성
@@ -11,9 +12,110 @@ USLA = USLA_model.USLA_Model(key_file_path, token_file_path, cano, acnt_prdt_cd)
 
 # 지난 USLA data 불러오기
 USLA_data = USLA.load_USLA_data()
+USD_adjust = 0.0 # 입출금 변동액 직접기입 기본 0$
 
 # date 업데이트
 current = datetime.now()
 current_date = current.date()
-USLA_data['date'] = current_date
 
+# 전일, 월초, 연초 전월말, 전년말 잔고 업데이트
+last_day_balance = USLA_data['balance']
+last_day_balance_KRW = USLA_data['balance_KRW']
+
+if current.day == 1: # 월초 전월 잔고 데이터 변경
+    last_month_balance = last_day_balance
+    last_month_balance_KRW = last_day_balance_KRW
+    KA.SendMessage(f"월초, 전월 잔고를 {last_month_balance}원으로 업데이트했습니다.")
+else:
+    last_month_balance = USLA_data['last_month_balance']
+    last_month_balance_KRW = USLA_data['last_month_balance_KRW']
+
+if current.month == 1 and current.day == 1: # 연초 전년 잔고 데이터 변경
+    last_year_balance = last_day_balance
+    last_year_balance_KRW = last_day_balance_KRW
+    KA.SendMessage(f"연초, 전년 잔고를 {last_year_balance}원으로 업데이트했습니다.")
+else:
+    last_year_balance = USLA_data['last_year_balance']
+    last_year_balance_KRW = USLA_data['last_year_balance_KRW']
+
+# 당일 평가잔고 산출
+Hold = USLA.get_total_balance()
+Hold_tickers = {}
+if len(Hold['stocks']) > 0:
+    for stock in Hold['stocks']:
+        ticker = stock['ticker']
+        qty = stock['quantity']
+        Hold_tickers[ticker] = qty
+else:
+    pass
+
+exchange_rate = Hold['exchange_rate']
+
+UPRO = Hold_tickers.get('UPRO', 0)
+TQQQ = Hold_tickers.get('TQQQ', 0)
+EDC = Hold_tickers.get('EDC', 0)
+TMF = Hold_tickers.get('TMF', 0)
+TMV = Hold_tickers.get('TMV', 0)
+CASH = USLA_data['CASH'] + USD_adjust
+
+# 당일 티커별 평가금 산출
+UPRO_eval = UPRO * (USLA.get_US_current_price('UPRO') * (1-0.0009))
+TQQQ_eval = TQQQ * (USLA.get_US_current_price('TQQQ') * (1-0.0009))
+EDC_eval = EDC * (USLA.get_US_current_price('EDC') * (1-0.0009))
+TMF_eval = TMF * (USLA.get_US_current_price('TMF') * (1-0.0009))
+TMV_eval = TMV * (USLA.get_US_current_price('TMV') * (1-0.0009))
+
+# 데이터 조정
+today_eval = UPRO_eval + TQQQ_eval + EDC_eval + TMF_eval + TMV_eval + CASH
+today_eval_KRW = today_eval * exchange_rate
+
+# 일, 월, 연 수익률 #
+daily_return = (today_eval - last_day_balance) / last_day_balance * 100
+daily_return = float("{:.2f}".format(daily_return))
+monthly_return = (today_eval - last_month_balance) / last_month_balance * 100
+monthly_return = float("{:.2f}".format(monthly_return))
+yearly_return = (today_eval - last_year_balance) / last_year_balance * 100
+yearly_return = float("{:.2f}".format(yearly_return))
+
+daily_return_KRW = (today_eval_KRW - last_day_balance_KRW) / last_day_balance_KRW * 100
+daily_return_KRW = float("{:.2f}".format(daily_return_KRW))
+monthly_return_KRW = (today_eval_KRW - last_month_balance_KRW) / last_month_balance_KRW * 100
+monthly_return_KRW = float("{:.2f}".format(monthly_return_KRW))
+yearly_return_KRW = (today_eval_KRW - last_year_balance_KRW) / last_year_balance_KRW * 100
+yearly_return_KRW = float("{:.2f}".format(yearly_return_KRW))
+
+# USLA data
+USLA_data = {
+    'date': str(current_date),
+    'regime_signal': USLA_data['regime_signal'],
+    'target_ticker1': USLA_data['target_ticker1'],
+    'target_weight1': USLA_data['target_weight1'],
+    'target_ticker2': USLA_data['target_ticker2'],
+    'target_weight2': USLA_data['target_weight2'],
+    'UPRO': UPRO,
+    'TQQQ': TQQQ,
+    'EDC': EDC,
+    'TMF': TMF,
+    'TMV': TMV,
+    'CASH': CASH,
+    'balance': today_eval,
+    'last_day_balance': last_day_balance,
+    'last_month_balance': last_month_balance,
+    'last_year_balance': last_year_balance,
+    'daily_return': daily_return,
+    'monthly_return': monthly_return,
+    'yearly_return': yearly_return,
+    'exchange_rate': exchange_rate,
+    'balance_KRW': today_eval_KRW,
+    'last_day_balance_KRW': last_day_balance_KRW,
+    'last_month_balance_KRW': last_month_balance_KRW,
+    'last_year_balance_KRW': last_year_balance_KRW,
+    'daily_return_KRW': daily_return_KRW,
+    'monthly_return_KRW': monthly_return_KRW,
+    'yearly_return_KRW': yearly_return_KRW
+}
+
+USLA.save_USLA_data_json(USLA_data)
+
+for i in len(USLA_ticker):
+    print(USLA_data[i])
